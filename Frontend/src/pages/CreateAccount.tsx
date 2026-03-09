edda6fddf71d667405dbe79b99b7c6a83acdc320
astrehlow <strehlow1@hotmail.com>
2026-03-09 08:53:39 -0500

    add create account component

--- Frontend/src/pages/CreateAccount.tsx
+++ Frontend/src/pages/CreateAccount.tsx
@@ -1,8 +1,65 @@
 export function CreateAccount() {
-    return (
-      <div>
-        <h1>Create Account</h1>
-        <p>This is the create account page.</p>
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          width: "900px",
+          height: "500px",
+          display: "flex",
+          borderRadius: "30px",
+          overflow: "hidden",
+          boxShadow: "0 4px 20px rgba(0, 0, 0, 0.25)",
+        }}
+      >
+        <div
+          style={{
+            flex: 1,
+            backgroundColor: "#73967f",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
+          }}
+        >
+          <div
+            style={{
+              textAlign: "center",
+            }}
+          >
+            <p
+              style={{
+                fontSize: "32px",
+                margin: "0",
+              }}
+            >
+              welcome to
+            </p>
+            <h1
+              style={{
+                fontSize: "85px",
+                margin: "0",
+              }}
+            >
+              QURIO
+            </h1>
+          </div>
+        </div>
+
+        <div
+          style={{
+            flex: 1,
+            backgroundColor: "#f5f5f5",
+          }}
+        ></div>
       </div>
-    )
-  }
+    </div>
+  );
+}
