822b6af8962c36b3525d341d58876e6b4b23be84
Indigenous1151 <nick.kolesar@yahoo.com>
2026-03-14 17:38:45 -0500

    Implemented create_account() in AuthService.

--- Backend/services/AuthService.py
+++ Backend/services/AuthService.py
@@ -1,6 +1,29 @@
+from models.PersonalInformation import PersonalInformation
+from models.PublicInformation import PublicInformation
+from models.User import User
+from UserRepository import UserRepository
+
+
 class AuthService:
+    def __init__(self, repo: UserRepository):
+        self.__repo = repo
+
     def create_account(self, username: str, email:str, password: str) -> User:
-        user_id = self.__repo.create_auth_user
+        user_id = self.__repo.create_auth_user(username, email, password)
+
+        personal_info = PersonalInformation(
+            user_id=user_id,
+            full_name="",
+            email=email
+        )
+
+        public_info = PublicInformation(
+            user_id=user_id,
+            username=username,
+            bio=""
+        )
+
+        return User(personal_info, public_info)
 
     def login(self, email: str, password: str) -> User:
         pass
