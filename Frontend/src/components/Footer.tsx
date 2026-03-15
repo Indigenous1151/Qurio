0ce82f1d7683eda5113e004823e4b271f77231a2
shreyasri258 <kertishreya2508@gmail.com>
2026-03-15 10:48:32 -0500

    added missing files

---
+++ Frontend/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+
+import {Link} from 'react-router-dom';
+import '../details/footer.css';
+
+export const Footer = () => {
+  return (
+    <div className = 'footer'>
+      <div>
+        <div>
+            Contact us at 1-XXX-XXXX or email us at ____
+        </div>
+      </div>
+    </div>
+  );
+};
