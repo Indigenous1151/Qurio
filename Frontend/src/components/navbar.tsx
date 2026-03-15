15141ed02546cef601045b4fae6d02a407b31282
kcbale <kcbale@ilstu.edu>
2026-03-15 11:17:29 -0500

    Updating missing file

--- Frontend/src/components/navbar.tsx
+++ Frontend/src/components/navbar.tsx
@@ -10,6 +10,9 @@ export const Navbar = () => {
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/UpdatePersonalInformation">Update Personal Information</Link></li>
+                <li><Link to="/UpdatePublicInformation">Update Public Information</Link></li>
+                <li><Link to="/create-account">Create Account</Link></li>
+                <li><Link to="/sign-in">Sign In</Link></li>
             </ul>
         </div>
       </div>
