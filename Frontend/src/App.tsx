f16414e4d1817f594b73718f424c2f586cede230
astrehlow <strehlow1@hotmail.com>
2026-03-15 02:17:19 -0500

    fix merge conflicts

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import {HashRouter as Router, Routes, Route} from 'react-router-dom' 
 import { Home } from './pages/Home'
 import { UpdatePersonalInformation } from './pages/UpdatePersonalInformation'
+import { UpdatePublicInformation } from './pages/UpdatePublicInformation'
+import { ResultScreen } from './pages/ResultScreen'
+import { TriviaGame } from './pages/TriviaGame'
+import { ClassicGame } from './pages/ClassicGame'
 import { CreateAccount } from './pages/CreateAccount'
 import { SignIn } from './pages/SignIn'
 import { ForgotPassword } from './pages/ForgotPassword'
 
 function App() {
-  // const [count, setCount] = useState(0)
+ 
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/UpdatePersonalInformation" element={<UpdatePersonalInformation/>} />
+        <Route path="/UpdatePublicInformation" element={<UpdatePublicInformation/>} />
+        <Route path="/game/setup" element={<ClassicGame />} />
+        <Route path="/game/play" element={<TriviaGame />} />
+        <Route path="/game/score" element={<ResultScreen />} />
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </Router>
     
-    // <>
-    // <div>
-    //   <h1>Welcome to Qurio!</h1>
-    // </div>
-
-    // </>
   )
 }
 
