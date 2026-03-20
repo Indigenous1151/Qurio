274c058478d224eb10bd403ef511300491339287
kcbale <kcbale@ilstu.edu>
2026-03-20 14:55:55 -0500

    Updating the uri for mongodb

--- Backend/database/MongoDBClient.py
+++ Backend/database/MongoDBClient.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+from env import MONGO_URI 
 
-uri = "mongodb+srv://qurio_db_user:zu9PLY2ufez799BK@qurioquestions.uuychwv.mongodb.net/?appName=QurioQuestions"
+uri = MONGO_URI
 
 # Create a MongoClient with Server API options
 client = MongoClient(
