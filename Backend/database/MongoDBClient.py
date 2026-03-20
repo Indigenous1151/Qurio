66d16e386fc5db3e268b855820dd7e8938377bf2
kcbale <kcbale@ilstu.edu>
2026-03-20 14:53:17 -0500

    Updated MongoDBCleint to be connected to the table

--- Backend/database/MongoDBClient.py
+++ Backend/database/MongoDBClient.py
@@ -1,20 +1,23 @@
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
-class MongoDBClient:
-    def __init__(self, url:str, db_name: str):
-        self.url = url
-        self.db_name = db_name
-        self.client = None
-        self.db = None
+uri = "mongodb+srv://qurio_db_user:zu9PLY2ufez799BK@qurioquestions.uuychwv.mongodb.net/?appName=QurioQuestions"
 
+# Create a MongoClient with Server API options
+client = MongoClient(
+    uri,
+    server_api=ServerApi(
+        version="1",
+        strict=True,
+        deprecation_errors=True
+    )
+)
 
-    def connect(self):
-        self.cleint = MongoClient(self.url)
-        self.db = self.client(self.db_name)
-    
-    def disconnect(self):
-        if self.client:
-            self.client.close()
-
-    def get_collection(self, name: str):
-        return self.db[name]
+def run():
+    try:
+        # Connect to the server
+        client.admin.command("ping")
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    finally:
+        # Close the connection
+        client.close()
