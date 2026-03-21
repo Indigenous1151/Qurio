767abbcd883968a847a8c3bb971d71530fce299a
shreyasri258 <kertishreya2508@gmail.com>
2026-03-21 15:53:53 -0500

    updated GameRepository to save game results in MongoDB

--- Backend/GameRepository.py
+++ Backend/GameRepository.py
@@ -2,15 +2,19 @@ from models.GameResult import GameResult
 
 class GameRepository:
     def __init__(self, db_client):
-        self.collection = db_client.get_collection("game_results")
-
-    def save(self, result: GameResult) -> bool:
-        try:
-            self.collection.insert_one(result.to_dict())
-            return True
-        except Exception as e:
-            print("Error saving result:", e)
-            return False
-
+        self.collection = db_client.get_collection("game_results") # saves data to game_results connection
     def get_results(self, user_id):
         return list(self.collection.find({"user": user_id}))
+
+    def save(self, result: GameResult) -> bool:
+        #print("save_result called")
+        try:
+            print(f"Saving result: {result.to_dict()}")
+            self.collection.insert_one(result.to_dict())
+            print("Result saved successfully!")
+            return True
+        except Exception as e:
+            print("Error:", e)
+            return False
+
+    
