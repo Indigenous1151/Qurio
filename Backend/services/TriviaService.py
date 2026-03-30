70e2487f6d285eed3a920063a810530f77b889c9
Indigenous1151 <nick.kolesar@yahoo.com>
2026-03-30 16:01:16 -0500

    Fixed category selection in backend for classic games.

--- Backend/services/TriviaService.py
+++ Backend/services/TriviaService.py
@@ -3,32 +3,24 @@ import requests
 class TriviaService:
     BASE_URL = "https://opentdb.com/api.php"
 
-    CATEGORY_MAPPING = {
-        "General Knowledge": 9,
-        "Entertainment: Books": 10,
-        "Entertainment: Film": 11,
-        "Entertainment: Music": 12,
-        "Entertainment: Musicals & Theatres": 13,
-        "Entertainment: Television": 14,
-        "Entertainment: Video Games": 15,
-        "Entertainment: Board Games": 16,
-        "Science & Nature": 17,
-        "Science: Computers": 18,
-        "Science: Mathematics": 19,
-        "Mythology": 20,
-        "Sports": 21,
-        "Geography": 22,
-        "History": 23,
-        "Politics": 24,
-        "Art": 25,
-        "Celebrities": 26,
-        "Animals": 27,
-        "Vehicles": 28,
-        "Entertainment: Comics": 29,
-        "Science: Gadgets": 30,
-        "Entertainment: Japanese Anime & Manga": 31,
-        "Entertainment: Cartoon & Animations": 32
-    }
+    # CATEGORY_MAPPING
+    #     "General Knowledge": 9,
+    #     "Books": 10,
+    #     "Film": 11,
+    #     "Music": 12,
+    #     "Television": 14,
+    #     "Video Games": 15,
+    #     "Science & Nature": 17,
+    #     "Computers": 18,
+    #     "Mathematics": 19,
+    #     "Mythology": 20,
+    #     "Sports": 21,
+    #     "Geography": 22,
+    #     "History": 23,
+    #     "Politics": 24,
+    #     "Art": 25,
+    #     "Animals": 27,
+    #
 
     def fetch_questions(self, amount=10, category=None, difficulty=None):
         #default number of questions will be 10 (if not specified by the user)
@@ -37,9 +29,7 @@ class TriviaService:
             "type": "multiple"
         }
         if category:
-            category_id = self.CATEGORY_MAPPING.get(category)
-            if category_id:
-                params["category"] = category_id
+            params["category"] = category
         if difficulty:
             params["difficulty"] = difficulty
 
