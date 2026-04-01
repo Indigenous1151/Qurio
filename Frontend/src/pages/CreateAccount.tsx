33dfc18eb5057cefbe7d01104bd6afbf03bef10a
astrehlow <strehlow1@hotmail.com>
2026-04-01 03:16:08 -0500

    update missed ports to 5001

--- Frontend/src/pages/CreateAccount.tsx
+++ Frontend/src/pages/CreateAccount.tsx
@@ -31,7 +31,7 @@ export function CreateAccount() {
     try {
       setIsLoading(true);
 
-      const response = await fetch("http://127.0.0.1:5000/auth/register", {
+      const response = await fetch("http://localhost:5001/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- Frontend/src/pages/ForgotPassword.tsx
+++ Frontend/src/pages/ForgotPassword.tsx
@@ -23,7 +23,7 @@ export function ForgotPassword() {
     try {
       setIsLoading(true);
 
-      const response = await fetch("http://127.0.0.1:5000/auth/forgot-password", {
+      const response = await fetch("http://localhost:5001/auth/forgot-password", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
