625f6274d5bd6e892f213b34be7c3ad973e89b3e
shreyasri258 <kertishreya2508@gmail.com>
2026-04-03 21:26:32 -0500

    Created a group invite model to control invites and aded service functions

--- Backend/services/GroupService.py
+++ Backend/services/GroupService.py
@@ -1,5 +1,6 @@
 from GroupRepository import GroupRepository
 from models.Group import Group
+from models.GroupInvite import GroupInvite
 
 class GroupService:
     def __init__(self, repo: GroupRepository, user_repo):
@@ -13,7 +14,39 @@ class GroupService:
             raise Exception("Failed to create group")
         return group
 
-    
-    
     def get_user_groups(self, user_id: str) -> list:
         return self.__repo.get_user_groups(user_id)
+
+    def get_group(self, group_id: str) -> dict:
+        group = self.__repo.get_group(group_id)
+        if not group:
+            raise Exception("Group not found")
+        return group
+
+    def invite_user(self, group_id: str, invited_by: str, username: str) -> bool:
+        results = self.__user_repo.search_by_username(username)
+        if not results:
+            raise Exception(f"User '{username}' not found")
+        receiver_id = results[0]["user_id"]
+        invite = GroupInvite(
+            group_id=group_id,
+            invited_by=invited_by,
+            invited_user=receiver_id
+        )
+        return self.__repo.save_invite(invite)
+
+    def get_pending_invites(self, user_id: str) -> list:
+        return self.__repo.get_pending_invites(user_id)
+
+    def accept_invite(self, invite_id: str, user_id: str) -> bool:
+        invites = self.__repo.get_pending_invites(user_id)
+        print(f"Invites: {invites}")
+        invite = next((i for i in invites if i["invite_id"] == invite_id), None)
+        print(f"Found invite: {invite}")
+        if not invite:
+            raise Exception("Invite not found")
+        self.__repo.update_invite_status(invite_id, "accepted")
+        return self.__repo.add_member(invite["group_id"], user_id)
+
+    def decline_invite(self, invite_id: str) -> bool:
+        return self.__repo.update_invite_status(invite_id, "declined")
