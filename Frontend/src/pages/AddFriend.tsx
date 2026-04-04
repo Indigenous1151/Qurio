0f206e702d457a4fd0eddf01c357e8eaeb75364a
shreyasri258 <kertishreya2508@gmail.com>
2026-04-03 21:41:25 -0500

    fixed a fetch URL

--- Frontend/src/pages/AddFriend.tsx
+++ Frontend/src/pages/AddFriend.tsx
@@ -30,7 +30,7 @@ export function AddFriend(){
         return;
       }
 
-      const response = await fetch("http://localhost:5000/friend/request", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/friend/request`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
