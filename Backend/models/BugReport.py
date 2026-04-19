1af15b66cf93461ec5dc00a1129272338572f983
kcbale <kcbale@ilstu.edu>
2026-04-19 12:04:48 -0500

    Backend logic for bug report. Adding the constructor, get_report, get_status, set_status, and to_dict

---
+++ Backend/models/BugReport.py
@@ -0,0 +1,28 @@
+import uuid
+
+class BugReport:
+    def __init__(self, sender_id: str, bug_report: str = "", status: str = "INCOMPLETE"):
+        self.report_id = str(uuid.uuid4())
+        self.sender_id = sender_id
+        self.bug_report = bug_report
+        self.status = status
+
+    def get_report(self) -> str:
+        return self.bug_report
+
+    def get_status(self) -> str:
+        return self.status
+
+    def set_status(self, status: str) -> None:
+        if status not in ["COMPLETE", "INCOMPLETE"]:
+            raise ValueError("Invalid Status")
+        else:
+            self.status = status
+
+    def to_dict(self) -> dict:
+        return {
+            "report_id": self.report_id,
+            "sender_id": self.sender_id,
+            "bug_report": self.bug_report,
+            "status": self.status,
+        }
