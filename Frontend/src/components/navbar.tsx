75063ad63d8cd836f43264ff8415098d7956d3cc
Indigenous1151 <nick.kolesar@yahoo.com>
2026-04-23 15:11:56 -0500

    Updated Navbar by grouping together like pages. This is to declutter the navbar itself and also make room for the admin section which will be conditionally rendered.

--- Frontend/src/components/navbar.tsx
+++ Frontend/src/components/navbar.tsx
@@ -1,26 +1,88 @@
-
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../details/Navbar.css';
 import { useState } from 'react';
 
 export const Navbar = () => {
-   const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [dropdown, setDropdown] = useState(null);
+
+  const handleNavClick = () => {
+    setOpen(false);
+    setDropdown(null);
+  };
+
+  const toggleDropdown = (name) => {
+    setDropdown(dropdown === name ? null : name);
+  };
+
   return (
     <div className="header">
-
       <div className="hamburger" onClick={() => setOpen(!open)}>
         ☰ Menu
       </div>
-       <ul className={`nav-list ${open ? "open" : ""}`}>
-        <li><Link to="/" onClick={() => setOpen(false)}>Home</Link></li>
-        <li><Link to = "/get-currency" onClick={() => setOpen(false)}>Purchase Currency</Link></li>
-        <li><Link to="/UpdatePersonalInformation" onClick={() => setOpen(false)}>Update Personal Information</Link></li>
-        <li><Link to="/UpdatePublicInformation" onClick={() => setOpen(false)}>Update Public Information</Link></li>
-        <li><Link to="/view-friend-list" onClick={() => setOpen(false)}>Friend List</Link></li>
-        <li><Link to="/groups" onClick={() => setOpen(false)}>Groups</Link></li>
-        <li><Link to="/personal-statistics" onClick={() => setOpen(false)}>Personal Statistics</Link></li>
-        <li><Link to="/view-notifications" onClick={() => setOpen(false)}>Notifications</Link></li>
-        <li><Link to="/logout" onClick={() => setOpen(false)}>Logout</Link></li>
+
+      <ul className={`nav-list ${open ? 'open' : ''}`}>
+
+        <li>
+          <Link to="/" onClick={handleNavClick}>Home</Link>
+        </li>
+
+        <li>
+          <Link to="/get-currency" onClick={handleNavClick}>
+            Purchase Currency
+          </Link>
+        </li>
+
+        {/* Profile */}
+        <li className="dropdown">
+          <span
+            className="nav-item"
+            onClick={() => toggleDropdown('profile')}
+          >
+            Profile
+          </span>
+
+          <ul className={`dropdown-menu ${dropdown === 'profile' ? 'show' : ''}`}>
+            <li><Link to="/UpdatePersonalInformation" onClick={handleNavClick}>Personal Info</Link></li>
+            <li><Link to="/UpdatePublicInformation" onClick={handleNavClick}>Public Info</Link></li>
+            <li><Link to="/personal-statistics" onClick={handleNavClick}>Statistics</Link></li>
+          </ul>
+        </li>
+
+        {/* Social */}
+        <li className="dropdown">
+          <span
+            className="nav-item"
+            onClick={() => toggleDropdown('social')}
+          >
+            Social
+          </span>
+
+          <ul className={`dropdown-menu ${dropdown === 'social' ? 'show' : ''}`}>
+            <li><Link to="/view-friend-list" onClick={handleNavClick}>Friends</Link></li>
+            <li><Link to="/groups" onClick={handleNavClick}>Groups</Link></li>
+            <li><Link to="/view-notifications" onClick={handleNavClick}>Notifications</Link></li>
+          </ul>
+        </li>
+
+        {/* Admin */}
+        <li className="dropdown">
+          <span
+            className="nav-item"
+            onClick={() => toggleDropdown('admin')}
+          >
+            Admin
+          </span>
+
+          <ul className={`dropdown-menu ${dropdown === 'admin' ? 'show' : ''}`}>
+            <li><Link to="/admin"></Link></li>
+          </ul>
+        </li>
+
+        <li>
+          <Link to="/logout" onClick={handleNavClick}>Logout</Link>
+        </li>
+
       </ul>
     </div>
   );
